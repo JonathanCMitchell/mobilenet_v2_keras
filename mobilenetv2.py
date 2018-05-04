73fbf7f41dc55085cf6bc5de091425965251872d
jonathancmitchell <jmitchell1991@gmail.com>
2018-05-04 13:52:07 -0700

    [feature] - uses FrozenBatchnorm for keras versions <= 2.1.3

--- mobilenetv2.py
+++ mobilenetv2.py
@@ -93,27 +93,15 @@ from keras.applications.imagenet_utils import _obtain_input_shape
 from keras.applications.imagenet_utils import decode_predictions
 from keras import backend as K
 
+from frozen_batchnorm import FrozenBatchNorm
+BatchNorm = FrozenBatchNorm
+
 # Load remote
 BASE_WEIGHT_PATH = 'https://github.com/JonathanCMitchell/mobilenet_v2_keras/releases/download/v1.1/'
 
 
 def relu6(x):
     return K.relu(x, max_value=6)
-
-
-# ==== Delete ==========
-class BatchNorm(BatchNormalization):
-    """Batch Normalization class. Subclasses the Keras BN class and
-    hardcodes training=False so the BN layer doesn't update
-    during training.
-
-    Batch normalization has a negative effect on training if batches are small
-    so we disable it here.
-    """
-
-    def call(self, inputs, training=None):
-       return super(self.__class__, self).call(inputs, training=False)
-
 
 class DepthwiseConv2D(Conv2D):
     """Depthwise separable 2D convolution.
